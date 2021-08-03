e7ca23483f3df48af6d1d5c2fd4f34ac8727180c
Radu Mereuta <headness13@gmail.com>
2021-08-03 22:52:52 +0300

    Make kprovex print more info when --verbose

--- kernel/src/main/java/org/kframework/kompile/DefinitionParsing.java
+++ kernel/src/main/java/org/kframework/kompile/DefinitionParsing.java
@@ -143,6 +143,7 @@ public class DefinitionParsing {
         def = Definition(def.mainModule(), modules.collect(Collections.toSet()), def.att());
 
         def = Kompile.excludeModulesByTag(excludeModules).apply(def);
+        sw.printIntermediate("Outer parsing [" + def.entryModules().size() + " modules]");
 
         errors = java.util.Collections.synchronizedSet(Sets.newHashSet());
         caches = loadCaches();
